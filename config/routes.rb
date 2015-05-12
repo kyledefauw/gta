24b8c78343a14fa795959d2b7e265caf366ed79e
Kyle DeFauw <kyle.defauw@gmail.com>
2015-05-11 20:35:51 -0600

    Set up routes and page files

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
+  resources :gardens do
+    resources :crops
+  end
+
+  resources :crops do
+    resources :plants
+  end
+
   get 'sign-up', to: 'registrations#new'
   post 'sign-up', to: 'registrations#create'
   get 'sign-in', to: 'sessions#new'
