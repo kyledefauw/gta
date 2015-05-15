a8af9c7316a006095888aa579ceaaa0a2701ddc0
Kyle DeFauw <kyle.defauw@gmail.com>
2015-05-14 22:14:39 -0600

    Fix params to match whats needed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:first_name, :last_name, :date_of_birth, :email, :password, :password_confirmation))
+    @user = User.new(params.require(:user).permit(:user_name, :email, :password, :password_confirmation))
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/', notice: "Account successfully created!"
