07bbe86420a75b998cc59a87a4699c606c3e6a90
Kyle DeFauw <kyle.defauw@gmail.com>
2015-05-23 01:08:55 -0600

    Start Tips page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
 
   root 'static_pages#index'
+  get '/tips', to: 'tips#index'
+  get '/bloom_state', to: 'tips#bloom_state'
+  get '/cloning', to: 'tips#cloning'
+  get '/curing', to: 'tips#curing'
+  get '/environment', to: 'tips#environment'
+  get '/pruning', to: 'tips#pruning'
+  get '/topping', to: 'tips#topping'
+  get '/veg_state', to: 'tips#veg_state'
+  get '/soil', to: 'tips#soil'
+  get '/hydroponics', to: 'tips#hydroponics'
+
+
 
   resources :gardens do
     resources :crops
@@ -18,7 +30,7 @@ Rails.application.routes.draw do
     resources :plant_images
   end
 
-  resources :plant_tasks do
+  resources :plants do
     resources :stage_of_growths
   end
 
@@ -26,7 +38,7 @@ Rails.application.routes.draw do
     resources :feedings
   end
 
-  resources :plant_tasks do
+  resources :plants do
     resources :general_notes
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523044015) do
+ActiveRecord::Schema.define(version: 20150523062031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,9 @@ ActiveRecord::Schema.define(version: 20150523044015) do
 
   create_table "general_notes", force: :cascade do |t|
     t.text     "notes"
-    t.integer  "plant_task_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer  "plant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "grow_medium", force: :cascade do |t|
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150523044015) do
     t.string   "plant_picture"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "plant_id"
   end
 
   create_table "plant_tasks", force: :cascade do |t|
